89098a1ba9d9423bc094bfc24fda4944048408d5
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-07-04 16:17:22 +0300

    add authorization route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrl.getAll);
-router.get("/:id", isValidId, ctrl.getById);
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
-router.delete("/:id", isValidId, ctrl.deleteById);
+router.get("/", authenticate, ctrl.getAll);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
+router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 router.patch(
-  "/:id/favorite",
+  "/:id/favorite", authenticate, 
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
