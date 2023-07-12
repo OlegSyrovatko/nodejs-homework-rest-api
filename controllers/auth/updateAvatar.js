d312b1a244730246f119cc52868d164645439795
Oleg <sirov@ukr.net>
2023-07-13 00:14:27 +0300

    add compressed unic image util

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -3,48 +3,28 @@ const { ctrlWrapper } = require("../../helpers");
 
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../public", "avatars");
 
-// const avatarsDir = __dirname.split("/").slice(0, -2).join("/") + "/public/avatars";
-// const avatarsDir = __dirname + "../../public/avatars";
-
 const updateAvatar = async (req, res) => {
-    const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
-    const resultUpload = path.join(avatarsDir, originalname);
-    // const resultUpload = avatarsDir + "/" + originalname;
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("../../public", "avatars", originalname);
-    // const avatarURL = "../../public/avatars/" + originalname;
-    await User.findByIdAndUpdate(_id, { avatarURL });
-    return res.status(200).json(avatarURL);
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`;
 
+  const resultUpload = path.join(avatarsDir, fileName);
+
+  await fs.rename(tempUpload, resultUpload);
+
+  const image = await Jimp.read(resultUpload);
+  image.resize(256, 256).write(resultUpload);
+
+  const avatarURL = path.join("../../public", "avatars", fileName);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  return res.status(200).json({ avatarURL });
 };
-
-
-// const { User } = require("../../models/user");
-// const { HttpError, ctrlWrapper } = require("../../helpers");
-
-// const updateAvatar = async (req, res) => {
-//   const { subscription = "starter" } = req.body;
-//   const { _id } = req.user;
-//   const data = await User.findByIdAndUpdate(
-//     _id,
-//     { subscription },
-//     { new: true }
-//   );
-
-//   if (!data) {
-//     throw HttpError(404, "Not found");
-//   }
-//   return res.status(200).json(data);
-// };
-
-
-
 
 module.exports = {
   updateAvatar: ctrlWrapper(updateAvatar),
-  // updateAvatar
 };
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -14,44 +14,4 @@ const upload = multer({
   storage: multerConfig,
 });
 
-
-
-
-
-
-// const multer = require("multer");
-// const path = require("path");
-
-// const tempDir = path.join(__dirname, "../", "temp");
-
-// const multerConfig = multer.diskStorage({
-//     destination: tempDir,
-//     filename: (req, file, cb) =>{
-//         cb(null, file.originalname);
-//     }
-// });
-
-// const upload = multer({
-//     storage: multerConfig
-// })
-
-// const multer = require("multer");
-
-// const tempDir = __dirname + "/../temp";
-
-// const multerConfig = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, tempDir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage: multerConfig,
-// });
-
-
 module.exports = upload;
-
