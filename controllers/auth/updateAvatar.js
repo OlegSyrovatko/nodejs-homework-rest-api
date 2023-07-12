83fd1e5a196990cc43b8611056d0b9fa1b895c64
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-07-12 17:13:09 +0300

    add avatar as filename

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,20 +1,50 @@
 const { User } = require("../../models/user");
 const { ctrlWrapper } = require("../../helpers");
-// const path = require("path");
-// const fs = require("fs/promises");
 
-// const avatarsDir = path.join(__dirname, "../../public", "avatars");
+const path = require("path");
+const fs = require("fs/promises");
+
+const avatarsDir = path.join(__dirname, "../../public", "avatars");
+
+// const avatarsDir = __dirname.split("/").slice(0, -2).join("/") + "/public/avatars";
+// const avatarsDir = __dirname + "../../public/avatars";
 
 const updateAvatar = async (req, res) => {
-  //   const { _id } = req.user;
-  //   const { path: tempUpload, originalname } = req.file;
-  //   const resultUpload = path.join(avatarsDir, originalname);
-  //   await fs.rename(tempUpload, resultUpload);
-  //   const avatarURL = path.join("../../public", "avatars", originalname);
-  //   await User.findByIdAndUpdate(_id, { avatarURL });
-  //   return res.status(200).json(avatarURL);
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const resultUpload = path.join(avatarsDir, originalname);
+    // const resultUpload = avatarsDir + "/" + originalname;
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("../../public", "avatars", originalname);
+    // const avatarURL = "../../public/avatars/" + originalname;
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    return res.status(200).json(avatarURL);
+
 };
+
+
+// const { User } = require("../../models/user");
+// const { HttpError, ctrlWrapper } = require("../../helpers");
+
+// const updateAvatar = async (req, res) => {
+//   const { subscription = "starter" } = req.body;
+//   const { _id } = req.user;
+//   const data = await User.findByIdAndUpdate(
+//     _id,
+//     { subscription },
+//     { new: true }
+//   );
+
+//   if (!data) {
+//     throw HttpError(404, "Not found");
+//   }
+//   return res.status(200).json(data);
+// };
+
+
+
 
 module.exports = {
   updateAvatar: ctrlWrapper(updateAvatar),
+  // updateAvatar
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,7 @@ const ctrl = require("../../controllers/auth");
 const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
+
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
@@ -13,18 +14,13 @@ router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
 
-// router.patch(
-//   "/subscription",
-//   authenticate,
-//   upload.single("avatar"),
-//   ctrl.updateAvatar
-// );
-
 router.patch(
-  "/avatars",
+  "/subscription",
   authenticate,
   validateBody(schemas.updateSubscription),
   ctrl.updateSubscription
 );
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
